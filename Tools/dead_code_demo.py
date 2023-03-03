0388832a25c6da872324ac6ae7acbc45ee1ea6c4
Dipam Soni <57869201+dipamsoni@users.noreply.github.com>
2023-03-03 18:18:12 +0530

    Add files via upload

---
+++ Tools/dead_code_demo.py
@@ -0,0 +1,16 @@
+def sum_func(arr):
+    return sum(arr)
+
+def max_func(arr):
+    return max(arr)
+
+if __name__ == "__main__":
+    input_arr = [521,598,463,789,159,357]
+    flag = 1
+    
+    input_sum = sun_func(input_arr)
+    print(input_sum)
+
+a = ''
+b = ''
+c = []
